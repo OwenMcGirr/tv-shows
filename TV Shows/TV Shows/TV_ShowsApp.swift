1f298f8011e6860a69b266a8aa4259408e985415
OwenMcGirr <o.a.mcgirr@gmail.com>
2021-07-05 16:11:46 +0100

    Add base project

---
+++ TV Shows/TV Shows/TV_ShowsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TV_ShowsApp.swift
+//  TV Shows
+//
+//  Created by Owen McGirr on 05/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct TV_ShowsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            HomeView()
+        }
+    }
+}
